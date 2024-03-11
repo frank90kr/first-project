a120a3d097e0ca0a1dd0da6ee9561a28d1402ea3
Francesco Gentile <francescogentilekr@gmail.com>
2024-03-11 17:30:56 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import ButtonComponent from "./components/ButtonComponent";
 import ImageComponent from "./components/ImageComponent";
+import TitleComponent from "./components/TitleComponent";
 
 function App() {
   return (
-    <div>
-      <ButtonComponent nameButton="Bottone" />
+    <div className="container">
+      <TitleComponent nameTitle="Il tuo Bau a portata di clik" />
+
+      <ButtonComponent nameButton="BAUUUU!" />
 
       <ImageComponent source="https://placedog.net/600" alt="Bau" />
     </div>
---
+++ src/components/TitleComponent.jsx
@@ -0,0 +1,5 @@
+const TitleComponent = function (props) {
+  return <h1 className="title">{props.nameTitle}</h1>;
+};
+
+export default TitleComponent;
